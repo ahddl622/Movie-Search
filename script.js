4479fdfedd357c329659685b2a245a9581ba4824
Seoweol <ahddl622@naver.com>
2024-01-08 21:18:03 +0900

    api 토큰 누락

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const options = {
   // API 요청을 위한 설정 객체
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer [Your API Key]'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODA4NmM3MDU2ZGQ3N2JjNzg1NDI3Mjg2MDgzYTQyZCIsInN1YiI6IjY1OTY0Y2Q1ODY5ZTc1NmZhYTA2OGIzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8FF4v9J3HjX0AGZo8cM9VfSfaGYfnYqLHUHW8KrBWCw",
+  },
 };
 
 let darkLayer = null;
@@ -15,7 +16,7 @@ function renderMovies(filteredMovies) {
   let movieList = document.querySelector(".movie-list");
   movieList.innerHTML = ""; // 기존 영화 목록 초기화
 
-  filteredMovies.forEach(movie => {
+  filteredMovies.forEach((movie) => {
     let movieCard = document.createElement("div");
     movieCard.innerHTML = `<div class="movie-card" id="${movie.id}">
       <h3 class="movie-name">${movie.title}</h3>
@@ -23,12 +24,12 @@ function renderMovies(filteredMovies) {
       <p class="movie-overview">${movie.overview}</p>
       <p class="movie-rating">Rating: ${movie.vote_average}</p>
     </div>`;
-    
+
     // 각 영화 카드에 클릭 이벤트 추가
     movieCard.addEventListener("click", () => {
       let movieModal = document.createElement("div");
       movieModal.classList.add("modal");
-      movieModal.classList.add("dark-layer")
+      movieModal.classList.add("dark-layer");
 
       movieModal.innerHTML = `<div class="card mb-3" id="modalCard">
       <div class="row g-0">
@@ -55,6 +56,7 @@ function renderMovies(filteredMovies) {
       });
 
       document.body.appendChild(movieModal);
+
       movieModal.style.display = "block";
     });
 
@@ -65,32 +67,41 @@ function renderMovies(filteredMovies) {
 
 // 입력된 쿼리와 일치하는 영화를 필터링하는 함수
 function searchMovies(query) {
-  const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(query.toLowerCase()));
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(query.toLowerCase())
+  );
   renderMovies(filteredMovies);
 }
 
 // 윈도우 로드 시 실행 할 이벤트 리스너
 window.addEventListener("load", function (event) {
   // 영화 데이터를 API에서 가져와서 처리
-  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(data => {
-    // 가져온 영화 데이터를 변수에 저장
-    movies = data.results;
-    
-    // 처음에는 모든 영화를 출력
-    renderMovies(movies);
 
-    // 검색 버튼 클릭 이벤트 처리
-    document.getElementById("search-button").addEventListener("click", function (event) {
-      event.preventDefault();
-      // 입력된 검색어를 가져와서 영화 검색
-      const searchInput = document.getElementById("searchInput");
-      const filterMovie = searchInput.value;
-      searchMovies(filterMovie);
-    });
-  })
-  .catch(err => console.error(err));
+  fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // 가져온 영화 데이터를 변수에 저장
+      movies = data.results;
+
+      // 처음에는 모든 영화를 출력
+      renderMovies(movies);
+
+      // 검색 버튼 클릭 이벤트 처리
+
+      document
+        .getElementById("search-button")
+        .addEventListener("click", function (event) {
+          event.preventDefault();
+          // 입력된 검색어를 가져와서 영화 검색
+          const searchInput = document.getElementById("searchInput");
+          const filterMovie = searchInput.value;
+          searchMovies(filterMovie);
+        });
+    })
+    .catch((err) => console.error(err));
 });
 
 function reload() {
